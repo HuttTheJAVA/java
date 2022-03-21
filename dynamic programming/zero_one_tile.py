f0cef14681a017d8f639cd7793dd48585cacbbb6
DESKTOP-B2F7HQT\user <chlghksdyd31@gmail.com>
2022-03-21 10:14:00 +0900

    baekjoon problem number 1904

---
+++ dynamic programming/zero_one_tile.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+d = [0]*1000001
+
+d[1] = 1
+d[2] = 2
+
+n = int(input())
+
+for i in range(3,n+1):
+    d[i] = (d[i-1]+d[i-2])%15746  # problem demand = %15746
+
+print(d[n])
